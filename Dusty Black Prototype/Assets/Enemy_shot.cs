d4eb9f6192bd1cd978ca5a77cd5c870e74d715d5
AresFernandez <ares.fernandez@enti.cat>
2019-10-01 19:24:38 +0200

    Unity Project first addition to repo

---
+++ Dusty Black Prototype/Assets/Enemy_shot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_shot : MonoBehaviour
+{
+    public float health = 100;
+
+    public Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void GotShot()
+    {
+        health -= 40;
+        if (health<=0)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    private IEnumerator Die()
+    {
+        //this.transform.Rotate(-75, 0, 0);
+        anim.SetTrigger("IsDead");
+
+
+        yield return new WaitForSeconds(1f);
+
+        Destroy(this.gameObject);
+    }
+
+
+
+}
---
+++ Dusty Black Prototype/Assets/Player_Shooter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shooter : MonoBehaviour
+{
+
+    private Camera playerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(playerCam.pixelWidth / 2, playerCam.pixelHeight / 2, 0);
+            Ray ray = playerCam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+
+                Enemy_shot target = hitObject.GetComponent<Enemy_shot>();
+
+                if (target != null)
+                {
+                    StartCoroutine(ShotGen(hit.point));
+                    target.GotShot();
+                }
+                else
+                {
+                    StartCoroutine(ShotGen(hit.point));
+                }
+                
+            }
+        }
+    }
+
+    private IEnumerator ShotGen(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = playerCam.pixelWidth / 2 - size / 4;
+        float posY = playerCam.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "+");
+    }
+
+}
