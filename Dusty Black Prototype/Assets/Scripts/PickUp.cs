00cd2b7ba344f80364ed7d5a3cf0e8faffa59f43
AresFernandez <ares.fernandez@enti.cat>
2019-10-19 13:25:13 +0200

    press e text added

--- Dusty Black Prototype/Assets/Scripts/PickUp.cs
+++ Dusty Black Prototype/Assets/Scripts/PickUp.cs
@@ -7,14 +7,32 @@ public class PickUp : MonoBehaviour
 
     public GameObject text;
     public GameObject ExitTrigger;
+    public GameObject PressEPanel;
 
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            PressEPanel.SetActive(false);
             text.SetActive(true);
             ExitTrigger.GetComponent<WinChecker>().missionDone = true;
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PressEPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PressEPanel.SetActive(false);
         }
     }
 }
