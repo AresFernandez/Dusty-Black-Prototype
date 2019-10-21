94e3e726bb67fdc1a87fdf43de2f33cf5272fc43
AresFernandez <ares.fernandez@enti.cat>
2019-10-21 17:38:39 +0200

    minor fix

--- Dusty Black Prototype/Assets/Scripts/MapSelector.cs
+++ Dusty Black Prototype/Assets/Scripts/MapSelector.cs
@@ -20,7 +20,7 @@ public class MapSelector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && GameObject.Find("TutorialPanel")==null)
+        if (Input.GetMouseButtonDown(0) && GameObject.Find("StrategyTutorialPanel(Clone)")==null)
         {
             Vector3 point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
             Ray ray = playerCam.ScreenPointToRay(point);
